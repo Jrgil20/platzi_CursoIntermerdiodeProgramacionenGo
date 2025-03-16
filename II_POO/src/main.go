5e30d5d8fb5baf1cf2344eeb4468b7f91da7bd15
Jesus Rodolfo Gil farias <131034722+Jrgil20@users.noreply.github.com>
2025-03-16 16:34:36 +0000

    Add clase7.md with methods and receiver functions in Go; update main.go with Employee struct methods

--- II_POO/src/main.go
+++ II_POO/src/main.go
@@ -1,27 +1,42 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Persona struct {
-	Nombre string
-	Edad   int
+type Employee struct {
+	// se crea una estructura con dos campos privados
+	id   int
+	name string
+}
+
+func (e *Employee) SetId(id int) {
+	// se crea un metodo para modificar el campo privado id
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	// se crea un metodo para modificar el campo privado name
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	// se crea un metodo para obtener el campo privado id
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	// se crea un metodo para obtener el campo privado name
+	return e.name
 }
 
 func main() {
-
-	// Crear una instancia de Persona
-	p := Persona{
-		Nombre: "Juan",
-		Edad:   30,
-	}
-	fmt.Printf("Nombre: %s, Edad: %d\n", p.Nombre, p.Edad)
-
-	// Otra instancia de Persona
-	p2 := Persona{
-		Nombre: "Maria",
-		Edad:   25,
-	}
-	fmt.Printf("Nombre: %s, Edad: %d\n", p2.Nombre, p2.Edad)
+	e := Employee{}
+	//fmt.Printf("%v", e)
+	e.id = 1
+	e.name = "Name"
+	//fmt.Printf("%v", e)
+	e.SetId(5)
+	e.SetName("Name 2")
+	//fmt.Printf("%v", e)
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
